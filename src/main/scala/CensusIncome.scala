a5cc53b5d2fe5dfdeb379000c08e8dd246a3edd6
Jia Guo <37137503+MrJale@users.noreply.github.com>
2020-12-17 16:14:05 -0500

    Update CensusIncome.scala

--- src/main/scala/CensusIncome.scala
+++ src/main/scala/CensusIncome.scala
@@ -35,14 +35,14 @@ object CensusIncome{
       .option("sep",",")
       .option("header","true")
       .option("inferSchema","true")
-      .csv("data/train.csv")
+      .csv(args(0))
       .as[myschema]
 
     val test=spark.read
       .option("sep",",")
       .option("header","true")
       .option("inferSchema","true")
-      .csv("data/test.csv")
+      .csv(args(1))
       .as[myschema]
 
     val cols = Array("age","workclass","fnlwgt","education","educationnum","maritalstatus","occupation","relationship","race",
