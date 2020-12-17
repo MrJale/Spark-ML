55324321442089c92813ee89080da0121af64092
Jia Guo <37137503+MrJale@users.noreply.github.com>
2020-12-17 17:51:30 -0500

    Update test_logis.sh

--- test_logis.sh
+++ test_logis.sh
@@ -5,5 +5,7 @@ mvn clean package
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /user/root/data/mllib/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./train.csv /user/root/data/mllib/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./test.csv /user/root/data/mllib/
-/usr/local/spark/bin/spark-submit --class CensusIncome --master=spark://$SPARK_MASTER:7077 target/LogisticRegressionWithElasticNetExample-1.0-SNAPSHOT.jar  
+/usr/local/spark/bin/spark-submit --class CensusIncome --master=spark://$SPARK_MASTER:7077 target/LogisticRegressionWithElasticNetExample-1.0-SNAPSHOT.jar hdfs://$SPARK_MASTER:9000/user/root/data/mllib/train.csv hdfs://$SPARK_MASTER:9000/user/root/data/mllib/test.csv
 mvn clean
+
+
