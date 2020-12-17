c38ddbbdec3c781abb12352cae1934e8fec2f081
Jia Guo <37137503+MrJale@users.noreply.github.com>
2020-12-17 17:04:00 -0500

    Update test_logis.sh

--- test_logis.sh
+++ test_logis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source env.sh
 mvn clean package
-/usr/local/hadoop/bin/hdfs dfs -rm -r /kmeans/input/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /kmeans/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./train.csv /kmeans/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./test.csv /kmeans/input/
-/usr/local/spark/bin/spark-submit --class CensusIncome --master=spark://$SPARK_MASTER:7077 target/CensusIncome-1.0-SNAPSHOT.jar hdfs://$SPARK_MASTER:9000/kmeans/input/ 
+/usr/local/hadoop/bin/hdfs dfs -rm -r /user/root/data/mllib/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /user/root/data/mllib/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./train.csv /user/root/data/mllib/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./test.csv /user/root/data/mllib/
+/usr/local/spark/bin/spark-submit --class CensusIncome --master=spark://$SPARK_MASTER:7077 target/LogisticRegressionWithElasticNetExample-1.0-SNAPSHOT.jar  
 mvn clean
