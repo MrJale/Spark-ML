9b4abb6582c96c0a49428262ac6b454293494b87
Jia Guo <37137503+MrJale@users.noreply.github.com>
2020-12-17 18:08:48 -0500

    Create test_randomforest.sh

---
+++ test_randomforest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source env.sh
+mvn clean package
+/usr/local/hadoop/bin/hdfs dfs -rm -r /user/root/data/mllib/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /user/root/data/mllib/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./train.csv /user/root/data/mllib/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./test.csv /user/root/data/mllib/
+/usr/local/spark/bin/spark-submit --class CensusIncome3 --master=spark://$SPARK_MASTER:7077 target/LogisticRegressionWithElasticNetExample-1.0-SNAPSHOT.jar hdfs://$SPARK_MASTER:9000/user/root/data/mllib/train.csv hdfs://$SPARK_MASTER:9000/user/root/data/mllib/test.csv
+mvn clean
